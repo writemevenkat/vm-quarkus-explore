c1bf91693f64f8f7071aeba062e6ee48e13c7ce0
Venkat <writemevenkat@localhost.localdomain>
2019-05-02 21:05:07 -0500

    Committer: Venkat <writemevenkat@localhost.localdomain>
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .classpath
    new file:   .dockerignore
    	new file:   .mvn/wrapper/MavenWrapperDownloader.java
    	new file:   .mvn/wrapper/maven-wrapper.jar
    new file:   .mvn/wrapper/maven-wrapper.properties
    	new file:   .project

---
+++ src/test/java/com/vm/explore/EventResourceTest.java
@@ -0,0 +1,17 @@
+package com.vm.explore;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class EventResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given().when().get("/event").then().statusCode(200).body(is("event"));
+    }
+
+}
